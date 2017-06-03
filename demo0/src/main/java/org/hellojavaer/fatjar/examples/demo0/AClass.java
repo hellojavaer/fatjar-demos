dafc5c026b286190857767a221718085fd5cc637
Kaiming Zou <hellojavaer@gmail.com>
2017-06-03 17:27:15 +0800

    add some demos

---
+++ demo0/src/main/java/org/hellojavaer/fatjar/examples/demo0/AClass.java
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 hellojavaer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.hellojavaer.fatjar.examples.demo0;
+
+import org.hellojavaer.fatjar.examples.utils.Utils;
+
+import java.net.URL;
+
+/**
+ *
+ * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 11/03/2017.
+ */
+public class AClass {
+
+    static {
+        System.out.println(AClass.class.getSimpleName() + " is loaded by " + AClass.class.getClassLoader());
+        URL location = AClass.class.getProtectionDomain().getCodeSource().getLocation();
+        System.out.println(AClass.class.getSimpleName() + "'s location is " +  location);
+    }
+
+    public void say(String msg) {
+        Utils.say(msg);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
